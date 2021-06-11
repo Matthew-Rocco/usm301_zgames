bbac109fe465683350068a8b16ed3e6f98b3efbe
Matthew <matt.rt30@gmail.com>
2021-06-10 20:22:08 -0400

    Generar interfaz y agregar axios

--- web/zgames/resources/views/ver_consolas.blade.php
+++ web/zgames/resources/views/ver_consolas.blade.php
@@ -1,5 +1,21 @@
 @extends("layouts.master")
 
 @section("contenido")
-    <h1>Hola soy ver consolas ekis de</h1>
+    <div class="row mt-5">
+        <div class="col-12 col-mb-12 col-lg-6 mx-auto">
+            <table class="table table-hover table-bordered table-striped table-responsive">
+                <thead class="bg-info">
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Marca</td>
+                        <td>Año de Lanzamiento</td>
+                        <td>Acciones</td>
+                    </tr>
+                </thead>
+                <tbody id="tbody-consola">
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
